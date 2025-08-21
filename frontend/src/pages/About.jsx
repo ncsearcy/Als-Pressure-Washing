6f5fcc1395a5776dad30a4c6a2899530653e2f2e
Nicholas Searcy <ncsearcy@gmail.com>
2025-08-20 21:34:53 -0500

    Updated the About Section to add a picture of Allen

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -56,7 +56,7 @@ const About = () => {
           <div className="col-lg-4">
             <div className="team-member-image text-center">
               <img 
-                src="/api/placeholder/300/300" 
+                src="/images/Allen.jpg" 
                 alt="Allen - Owner" 
                 className="img-fluid rounded-circle shadow"
                 style={{width: '250px', height: '250px', objectFit: 'cover'}}
